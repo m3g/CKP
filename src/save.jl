98f217213555135e4cb6d8c67f85a3ff3d242d54
Leandro Martinez <leandro@iqm.unicamp.br>
2020-04-23 15:03:07 -0300

    read other data of save traj correctly

--- src/save.jl
+++ src/save.jl
@@ -28,16 +28,22 @@ function read_wg(filename :: String)
   f = open(filename,"r")
   S = JSON3.read(f,SaveData)
   traj = Traj(S.input.n,S.input.nsave)
+  # Need to convert the Traj structure because the coordinates
+  # are read in a single column, instead of the [n,2] array we want.
   for i in 1:S.input.nsave 
     for iat in 1:S.input.n
       traj.atoms[i].x[iat,1] = S.traj.atoms[i].x[iat]
       traj.atoms[i].x[iat,2] = S.traj.atoms[i].x[iat+S.input.n]
       traj.atoms[i].status[iat] = S.traj.atoms[i].status[iat]
     end
+    traj.potential[i] = S.traj.potential[i]
+    traj.kinetic[i] = S.traj.kinetic[i]
+    traj.total[i] = S.traj.total[i]
+    traj.time[i] = S.traj.time[i]
+    traj.nenc[i] = S.traj.nenc[i]
   end
   input = S.input
   S = nothing
   return input, traj
 end
-
 
